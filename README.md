5c553c644fd99d3a1028fe0a9900e0508deabeed
ammiranda <alexandermichaelmiranda@gmail.com>
2016-07-12 15:09:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+CS 340 Introduction to Databases
+===
+
+This repository contains my assignment work for Oregon State University's Introduction to Databases course.
---
+++ assignments/2/Assignment2-selection-5.sql
@@ -0,0 +1,27 @@
+#1 Find all films with maximum length and minimum rental duration (compared to all other films). 
+#In other words let L be the maximum film length, and let R be the minimum rental duration in the table film. You need to find all films with length L and rental duration R.
+#You just need to return attribute film id for this query.
+
+
+
+#2 We want to find out how many actors have played in each film, so for each film return the film id, film title, and the number of actors who played in that film. Some films may have no actors, and your query does not need to return those films.
+
+
+
+#3 Find the average length of films for each language. Your query should return every language even if there is no films in that language. language refers to attribute language_id (not attribute original_language_id)
+
+
+
+#4 We want to find out how many of each category of film KEVIN BLOOM has started in so return a table with category.name and the count
+#of the number of films that KEVIN was in which were in that category order by the category name ascending (Your query should return every category even if KEVIN has been in no films in that category).
+
+
+
+#5 Find the film title of all films which do not feature both SCARLETT DAMON and BEN HARRIS(so you will not list a film if both of these actors have played in that film, but if only one or none of these actors have played in a film, that film should be listed).
+#Order the results by title, descending (use ORDER BY title DESC at the end of the query)
+#Warning, this is a tricky one and while the syntax is all things you know, you have to think oustide
+#the box a bit to figure out how to get a table that shows pairs of actors in movies
+
+
+
+
