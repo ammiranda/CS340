f91f491b407aeeb7ea0f300f21bac522291a29fe
ammiranda <alexandermichaelmiranda@gmail.com>
2016-07-31 18:31:31 -0400

    Added web and written directory for final project

--- assignments/final_project/web/server.js
+++ assignments/final_project/web/server.js
@@ -13,12 +13,14 @@ var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 4000);
+app.set('port', 3000);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-
+app.get('/', function(req, res) {
+	res.render('home');
+});
 
 app.use(function(req, res) {
 	res.status(404);
---
+++ assignments/final_project/written/project_text_answers.md
@@ -0,0 +1,3 @@
+### Outline
+
+The database content will be all of the relevant information regarding the Star Trek TV series. The database will have tables containing data about characters, actors, episodes, and studios that were involved with Star Trek productions. 
