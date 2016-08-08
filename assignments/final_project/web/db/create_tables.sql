9f9f8436b2ad90e5e55f02392259ed1ee24c23ff
ammiranda <alexandermichaelmiranda@gmail.com>
2016-08-07 20:45:30 -0700

    Added comments to the create and seed sql scripts

--- assignments/final_project/web/db/create_tables.sql
+++ assignments/final_project/web/db/create_tables.sql
@@ -1,3 +1,8 @@
+/*
+ * Drop statements to clean the out the old star_trek database records
+ * if they exist.
+*/
+
 DROP TABLE IF EXISTS `actor_character`;
 DROP TABLE IF EXISTS `character_episode`;
 DROP TABLE IF EXISTS `actor_series`;
@@ -7,13 +12,23 @@ DROP TABLE IF EXISTS `st_character`;
 DROP TABLE IF EXISTS `series`;
 DROP TABLE IF EXISTS `studio`;
 
+/*
+ * star_trek.actor
+ * Actors that have appeared in one or many Star Trek TV shows
+*/
+
 CREATE TABLE `actor` (
    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
    `fname` VARCHAR(255) NOT NULL,
-   `lname` VARCHAR(255) NOT NULL,
+   `lname` VARCHAR(255),
    `website` VARCHAR(255),
    `twitter` VARCHAR(255)	
 ) ENGINE=’innoDB’;
+
+/*
+ * star_trek.st_character
+ * Character roles that have appeared in one or many Star Trek TV shows
+*/
 
 CREATE TABLE `st_character` (
    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
@@ -22,12 +37,22 @@ CREATE TABLE `st_character` (
    `race` VARCHAR(255) NOT NULL
 ) ENGINE=’innoDB’;
 
+/*
+ * star_trek.studio
+ * Studios that have produced one or many Star Trek TV shows
+*/
+
 CREATE TABLE `studio` (
    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
    `name` VARCHAR(255) NOT NULL,
    `address` VARCHAR(255),
    `website` VARCHAR(255)
 ) ENGINE=’innoDB’;
+
+/*
+ * star_trek.series
+ * Star Trek TV series
+*/
 
 CREATE TABLE `series` (
    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
@@ -42,6 +67,11 @@ CREATE TABLE `series` (
       ON UPDATE CASCADE
 ) ENGINE=’innoDB’;
 
+/*
+ * star_trek.episode
+ * Star Trek TV episodes
+*/
+
 CREATE TABLE `episode` (
    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
    `title` VARCHAR(255) NOT NULL,
@@ -53,6 +83,12 @@ CREATE TABLE `episode` (
       ON DELETE CASCADE
       ON UPDATE CASCADE
 ) ENGINE=’innoDB’;
+
+/*
+ * star_trek.actor_series
+ * This table relates actors to the Star Trek series they have
+ * acted in. Many to many relationship (actor, series)
+*/
 
 CREATE TABLE `actor_series` (
    `actor_id` INT(11),
@@ -66,6 +102,12 @@ CREATE TABLE `actor_series` (
       ON UPDATE CASCADE
 ) ENGINE=’innoDB’;
 
+/*
+ * star_trek.actor_character
+ * This table relates actors to the characters they have played
+ * in Star Trek TV shows. Many to many relationship (actor, character)
+*/
+
 CREATE TABLE `actor_character` (
    `actor_id` INT(11),
    `character_id` INT(11),
@@ -77,6 +119,12 @@ CREATE TABLE `actor_character` (
       ON DELETE CASCADE
       ON UPDATE CASCADE
 ) ENGINE=’innoDB’;
+
+/*
+ * star_trek.character_episode
+ * This table relates Star Trek characters to which episodes they
+ * appeared in. Many to many relationship (character, episode)
+*/
 
 CREATE TABLE `character_episode` (
    `character_id` INT(11),
--- assignments/final_project/web/db/seed.sql
+++ assignments/final_project/web/db/seed.sql
@@ -1,3 +1,11 @@
+/*
+ * The statements below are used to prepopulate the star_trek database with data
+*/
+
+/*
+ * Prepopulating the table actor with Star Trek actors
+*/
+
 INSERT INTO actor (fname, lname, website, twitter)
 VALUES ("Patrick", "Stewart", NULL, "@SirPatStew"),
        ("Leonard", "Nimoy", "https://www.shopllap.com", "@TheRealNimoy"),
@@ -6,6 +14,9 @@ VALUES ("Patrick", "Stewart", NULL, "@SirPatStew"),
        ("Brent", "Spiner", "http://www.therealbrentspiner.com", "@BrentSpiner"),
        ("Colm", "Meaney", NULL, NULL),
        ("Michael", "Dorn", NULL, "@akaWorf");
+/*
+ * Prepopulating the table st_character with Star Trek characters
+*/
 
 INSERT INTO st_character (fname, lname, race)
 VALUES ("Jean Luc", "Picard", "Human"),
@@ -16,17 +27,29 @@ VALUES ("Jean Luc", "Picard", "Human"),
        ("Miles", "O'Brien", "Human"),
        ("Worf", NULL, "Klingon");
 
+/*
+ * Prepopulating the table studio with studios used in Star Trek productions
+*/
+
 INSERT INTO studio (name, address, website)
 VALUES ("Desilu Productions", "Los Angeles, CA", NULL),
        ("Paramount Domestic Television", "5555 Melrose Avenue, Hollywood, CA, 90038", "http://www.paramountstudios.com/"),
        ("Paramount Network Television", "5555 Melrose Avenue, Hollywood, CA, 90038", "http://www.paramountstudios.com/"),
        ("CBS Television", "7800 Beverly Boulevard, Los Angeles, CA, 90036", "http://www.cbstelevisioncity.com");
 
+/*
+ * Prepopulating the table series with the various Star Trek TV series
+*/
+
 INSERT INTO series (title, creator_fname, creator_lname, start_date, end_date, studio_id)
 VALUES ("Star Trek: The Original Series", "Gene", "Roddenberry", '1966-09-08', '1969-06-03', (SELECT id FROM studio WHERE name = "Desilu Productions")),
        ("Star Trek: The Next Generation", "Gene", "Roddenberry", '1987-09-28', '1994-05-23', (SELECT id FROM studio WHERE name = "Paramount Domestic Television")),
        ("Star Trek: Voyager", "Rick", "Berman", '1995-01-16', '2001-05-23', (SELECT id FROM studio WHERE name = "Paramount Network Television")),
        ("Star Trek: Deep Space Nine", "Rick", "Berman", '1993-01-03', '1999-06-02', (SELECT id FROM studio WHERE name = "Paramount Domestic Television"));
+
+/*
+ * Prepopulating the table episode with some Star Trek episodes
+*/
 
 INSERT INTO episode (title, episode_number, season_number, air_date, series_id)
 VALUES ("The Trouble with Tribbles", 15, 2, '1967-12-29', (SELECT id FROM series WHERE title = "Star Trek: The Original Series")),
@@ -35,38 +58,83 @@ VALUES ("The Trouble with Tribbles", 15, 2, '1967-12-29', (SELECT id FROM series
        ("The Swarm", 4, 3, '1996-09-25', (SELECT id FROM series WHERE title = "Star Trek: Voyager")),
        ("Inquisition", 18, 6, '1998-04-08', (SELECT id FROM series WHERE title = "Star Trek: Deep Space Nine"));
 
+/*
+ * Prepopulating the actor_series table to relate actors to Star Trek Series they have
+ * appeared in.
+*/
+
 INSERT INTO actor_series (actor_id, series_id)
-VALUES ((SELECT id FROM actor WHERE fname = "Patrick" AND lname = "Stewart"), (SELECT id FROM series WHERE title = "Star Trek: The Next Generation")),
-       ((SELECT id FROM actor WHERE fname = "Patrick" AND lname = "Stewart"), (SELECT id FROM series WHERE title = "Star Trek: Deep Space Nine")),
-       ((SELECT id FROM actor WHERE fname = "Leonard" AND lname = "Nimoy"), (SELECT id FROM series WHERE title = "Star Trek: The Original Series")),
-       ((SELECT id FROM actor WHERE fname = "Michael" AND lname = "Dorn"), (SELECT id FROM series WHERE title = "Star Trek: The Next Generation")),
-       ((SELECT id FROM actor WHERE fname = "Michael" AND lname = "Dorn"), (SELECT id FROM series WHERE title = "Star Trek: Deep Space Nine")),
-       ((SELECT id FROM actor WHERE fname = "Colm" AND lname = "Meaney"), (SELECT id FROM series WHERE title = "Star Trek: The Next Generation")),
-       ((SELECT id FROM actor WHERE fname = "Colm" AND lname = "Meaney"), (SELECT id FROM series WHERE title = "Star Trek: Deep Space Nine")),
-       ((SELECT id FROM actor WHERE fname = "William" AND lname = "Shatner"), (SELECT id FROM series WHERE title = "Star Trek: The Original Series")),
-       ((SELECT id FROM actor WHERE fname = "Brent" AND lname = "Spiner"), (SELECT id FROM series WHERE title = "Star Trek: The Next Generation")),
-       ((SELECT id FROM actor WHERE fname = "Kate" AND lname = "Mulgrew"), (SELECT id FROm series WHERE title = "Star Trek: Voyager"));
+VALUES ((SELECT id FROM actor WHERE fname = "Patrick" AND lname = "Stewart"),
+              (SELECT id FROM series WHERE title = "Star Trek: The Next Generation")),
+       ((SELECT id FROM actor WHERE fname = "Patrick" AND lname = "Stewart"),
+              (SELECT id FROM series WHERE title = "Star Trek: Deep Space Nine")),
+       ((SELECT id FROM actor WHERE fname = "Leonard" AND lname = "Nimoy"),
+              (SELECT id FROM series WHERE title = "Star Trek: The Original Series")),
+       ((SELECT id FROM actor WHERE fname = "Michael" AND lname = "Dorn"),
+              (SELECT id FROM series WHERE title = "Star Trek: The Next Generation")),
+       ((SELECT id FROM actor WHERE fname = "Michael" AND lname = "Dorn"),
+              (SELECT id FROM series WHERE title = "Star Trek: Deep Space Nine")),
+       ((SELECT id FROM actor WHERE fname = "Colm" AND lname = "Meaney"),
+              (SELECT id FROM series WHERE title = "Star Trek: The Next Generation")),
+       ((SELECT id FROM actor WHERE fname = "Colm" AND lname = "Meaney"),
+              (SELECT id FROM series WHERE title = "Star Trek: Deep Space Nine")),
+       ((SELECT id FROM actor WHERE fname = "William" AND lname = "Shatner"),
+              (SELECT id FROM series WHERE title = "Star Trek: The Original Series")),
+       ((SELECT id FROM actor WHERE fname = "Brent" AND lname = "Spiner"),
+              (SELECT id FROM series WHERE title = "Star Trek: The Next Generation")),
+       ((SELECT id FROM actor WHERE fname = "Kate" AND lname = "Mulgrew"),
+              (SELECT id FROm series WHERE title = "Star Trek: Voyager"));
+
+/*
+ * Prepopulating the actor_character table to relate actors to Star Trek characters they
+ * have portrayed.
+*/
 
 INSERT INTO actor_character (actor_id, character_id)
-VALUES ((SELECT id FROM actor WHERE fname = "Patrick" AND lname = "Stewart"), (SELECT id FROM st_character WHERE fname = "Jean Luc" AND lname = "Picard")),
-       ((SELECT id FROM actor WHERE fname = "Leonard" AND lname = "Nimoy"), (SELECT id FROM st_character WHERE fname = "Spock")),
-       ((SELECT id FROM actor WHERE fname = "Michael" AND lname = "Dorn"), (SELECT id FROM st_character WHERE fname = "Worf")),
-       ((SELECT id FROM actor WHERE fname = "Colm" AND lname = "Meaney"), (SELECT id FROM st_character WHERE fname = "Miles" AND lname = "O'Brien")),
-       ((SELECT id FROM actor WHERE fname = "Brent" AND lname = "Spiner"), (SELECT id FROM st_character WHERE fname = "Data")),
-       ((SELECT id FROM actor WHERE fname = "Kate" AND lname = "Mulgrew"), (SELECT id FROM st_character WHERE fname = "Katherine" AND lname = "Janeway")),
-       ((SELECT id FROM actor WHERE fname = "William" AND lname = "Shatner"), (SELECT id FROM st_character WHERE fname = "James" AND lname = "Kirk"));
+VALUES ((SELECT id FROM actor WHERE fname = "Patrick" AND lname = "Stewart"),
+              (SELECT id FROM st_character WHERE fname = "Jean Luc" AND lname = "Picard")),
+       ((SELECT id FROM actor WHERE fname = "Leonard" AND lname = "Nimoy"),
+              (SELECT id FROM st_character WHERE fname = "Spock")),
+       ((SELECT id FROM actor WHERE fname = "Michael" AND lname = "Dorn"),
+              (SELECT id FROM st_character WHERE fname = "Worf")),
+       ((SELECT id FROM actor WHERE fname = "Colm" AND lname = "Meaney"),
+              (SELECT id FROM st_character WHERE fname = "Miles" AND lname = "O'Brien")),
+       ((SELECT id FROM actor WHERE fname = "Brent" AND lname = "Spiner"),
+              (SELECT id FROM st_character WHERE fname = "Data")),
+       ((SELECT id FROM actor WHERE fname = "Kate" AND lname = "Mulgrew"), 
+              (SELECT id FROM st_character WHERE fname = "Katherine" AND lname = "Janeway")),
+       ((SELECT id FROM actor WHERE fname = "William" AND lname = "Shatner"),
+              (SELECT id FROM st_character WHERE fname = "James" AND lname = "Kirk"));
+
+/*
+ * Prepopulating the character_episode table to relate Star Trek characters to episodes
+ * they have appeared in.
+*/
 
 INSERT INTO character_episode (character_id, episode_id)
-VALUES ((SELECT id FROM st_character WHERE fname = "Jean Luc" AND lname = "Picard"), (SELECT id FROM episode WHERE title = "Ensign Ro")),
-       ((SELECT id FROM st_character WHERE fname = "Data"), (SELECT id FROM episode WHERE title = "Ensign Ro")),
-       ((SELECT id FROM st_character WHERE fname = "Data"), (SELECT id FROM episode WHERE title = "Legacy")),
-       ((SELECT id FROM st_character WHERE fname = "Jean Luc" AND lname = "Picard"), (SELECT id FROM episode WHERE title = "Legacy")),
-       ((SELECT id FROM st_character WHERE fname = "Worf"), (SELECT id FROM episode WHERE title = "Legacy")),
-       ((SELECT id FROM st_character WHERE fname = "Worf"), (SELECT id FROM episode WHERE title = "Ensign Ro")),
-       ((SELECT id FROM st_character WHERE fname = "Worf"), (SELECT id FROM episode WHERE title = "Inquisition")),
-       ((SELECT id FROM st_character WHERE fname = "Miles" AND lname = "O'Brien"), (SELECT id FROM episode WHERE title = "Legacy")),
-       ((SELECT id FROM st_character WHERE fname = "Miles" AND lname = "O'Brien"), (SELECT id FROM episode WHERE title = "Ensign Ro")),
-       ((SELECT id FROM st_character WHERE fname = "Miles" AND lname = "O'Brien"), (SELECT id FROM episode WHERE title = "Inquisition")),
-       ((SELECT id FROM st_character WHERE fname = "Katherine" AND lname = "Janeway"), (SELECT id FROM episode WHERE title = "The Swarm")),
-       ((SELECT id FROM st_character WHERE fname = "James" AND lname = "Kirk"), (SELECT id FROM episode WHERE title = "The Trouble with Tribbles")),
-       ((SELECT id FROM st_character WHERE fname = "Spock"), (SELECT id FROM episode WHERE title = "The Trouble with Tribbles"));
+VALUES ((SELECT id FROM st_character WHERE fname = "Jean Luc" AND lname = "Picard"),
+              (SELECT id FROM episode WHERE title = "Ensign Ro")),
+       ((SELECT id FROM st_character WHERE fname = "Data"),
+              (SELECT id FROM episode WHERE title = "Ensign Ro")),
+       ((SELECT id FROM st_character WHERE fname = "Data"),
+              (SELECT id FROM episode WHERE title = "Legacy")),
+       ((SELECT id FROM st_character WHERE fname = "Jean Luc" AND lname = "Picard"),
+              (SELECT id FROM episode WHERE title = "Legacy")),
+       ((SELECT id FROM st_character WHERE fname = "Worf"),
+              (SELECT id FROM episode WHERE title = "Legacy")),
+       ((SELECT id FROM st_character WHERE fname = "Worf"),
+              (SELECT id FROM episode WHERE title = "Ensign Ro")),
+       ((SELECT id FROM st_character WHERE fname = "Worf"),
+              (SELECT id FROM episode WHERE title = "Inquisition")),
+       ((SELECT id FROM st_character WHERE fname = "Miles" AND lname = "O'Brien"),
+              (SELECT id FROM episode WHERE title = "Legacy")),
+       ((SELECT id FROM st_character WHERE fname = "Miles" AND lname = "O'Brien"),
+              (SELECT id FROM episode WHERE title = "Ensign Ro")),
+       ((SELECT id FROM st_character WHERE fname = "Miles" AND lname = "O'Brien"),
+              (SELECT id FROM episode WHERE title = "Inquisition")),
+       ((SELECT id FROM st_character WHERE fname = "Katherine" AND lname = "Janeway"),
+              (SELECT id FROM episode WHERE title = "The Swarm")),
+       ((SELECT id FROM st_character WHERE fname = "James" AND lname = "Kirk"),
+              (SELECT id FROM episode WHERE title = "The Trouble with Tribbles")),
+       ((SELECT id FROM st_character WHERE fname = "Spock"),
+              (SELECT id FROM episode WHERE title = "The Trouble with Tribbles"));
